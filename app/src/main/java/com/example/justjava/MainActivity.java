80b68bccbacff083101c41a5c29147ba5985e77a
SARKAR <sshankhadeep226@gmail.com>
2020-12-22 17:43:44 +0530

    Drinkapp

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -12,6 +13,8 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 int quantity = 2;
 String priceMessage="Total";
+EditText dash = (EditText) findViewById(R.id.name);
+String value= dash.getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,7 @@ String priceMessage="Total";
     }
 
     public void sendMessage(View view){
-String priceMessage = "Total:" +   quantity * 5  +  "\n Thank You!";
+String priceMessage = value +"Total:" +   quantity * 5  +  "\n Thank You!";
 CheckBox addwhipped= (CheckBox) findViewById(R.id.whipped);
 boolean checked =addwhipped.isChecked();
 priceMessage += "\n whipped cream added" + checked;
@@ -37,9 +40,9 @@ displa(priceMessage,checked);
 //    }
 
 
-    private void displa(String message,  boolean checked) {
+    private void displa(String message,  boolean checked , String name) {
         TextView priceTextView=(TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(String.format("%s%s", message, checked));
+        priceTextView.setText(String.format("%s%s", message, checked,name));
     }
 
 
